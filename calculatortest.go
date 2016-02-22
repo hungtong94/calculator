467b8d1216fb53ebcb0254f33794066b56a9d380
hungtong94 <hughoi94@gmail.com>
2016-02-22 15:04:06 +1100

    First commit

---
+++ calculatortest.go
@@ -0,0 +1,21 @@
+package calculator
+
+import (
+	"testing"
+)
+
+func Add(x int64, y int64) int64 {
+	return x + y
+}
+
+func Substract(x int64, y int64) int64 {
+	return x - y
+}
+
+func Division(x int64, y int64) int64 {
+	return x / y
+}
+
+func Mutiple(x int64, y int64) int64 {
+	return x * y
+}
